5cfc592e3a3171f17670f8c196caad7421376315
mohammadyahyaq <m7md2012g@gmail.com>
2020-11-30 16:34:32 +0300

    adding php file

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MyCV</title>
+  </head>
+  <body>
+    <div class="header">
+      <h1 class="logo">MyCV</h1>
+    </div>
+  </body>
+</html>
