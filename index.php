18a1321fbce7f878c3f939ae8cb4ae8f55ca9819
mohammadyahyaq <m7md2012g@gmail.com>
2020-11-30 20:05:33 +0300

    first version header

--- index.php
+++ index.php
@@ -3,6 +3,17 @@
   <head>
     <meta charset="utf-8">
     <title>MyCV</title>
+
+    <!-- The required stylesheet -->
+    <link rel="stylesheet" href="stylesheets/main.css">
+
+    <!-- import all the used fonts for the project -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@300;600;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&display=swap" rel="stylesheet">
+
+
   </head>
   <body>
     <div class="header">
