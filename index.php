dbb439fffc9c2ef265f5b0ca18ab9c0f92d96182
mohammadyahyaq <m7md2012g@gmail.com>
2020-12-04 17:07:45 +0300

    header first version

--- index.php
+++ index.php
@@ -13,11 +13,21 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@500&display=swap" rel="stylesheet">
 
+    <!-- import the icons used in the website -->
+    <script src="https://kit.fontawesome.com/adac5f140b.js" crossorigin="anonymous"></script>
 
   </head>
   <body>
     <div class="header">
       <h1 class="logo">MyCV</h1>
+      <div class="tabs-container">
+        <div class="tab-button">
+          <h2 class="tab">Home</h2>
+        </div>
+        <div class="tab-button">
+          <h2 class="tab">About Us</h2>
+        </div>
+      </div>
     </div>
   </body>
 </html>
